31f83220b6bb849f5bacb4ed28962dde2e91f555
preston176 <prestonnyamweya@gmail.com>
2023-08-02 15:01:45 +0300

    db setup

--- chatsphere-backend/index.js
+++ chatsphere-backend/index.js
@@ -10,6 +10,15 @@ app.use(cors())
 
 app.use(express.json());
 
+//connect mongoose
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB CONNECTION IS SUCCESSFUL")
+}).catch((err) => console.log(err.message))
+
+
 const server = app.listen(process.env.PORT, () => {
     console.log(`server is up and running on ${process.env.PORT}`)
 })
