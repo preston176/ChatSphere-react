031bcf216687912fde08605d3b0d0807b1a57fb5
preston176 <prestonnyamweya@gmail.com>
2023-08-02 15:26:48 +0300

    dm sans hanken grotesk and poppins fonts

--- chatsphere-frontend/src/pages/Register.jsx
+++ chatsphere-frontend/src/pages/Register.jsx
@@ -1,10 +1,33 @@
-
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("form")
+  }
+const handleChange = (e) => {
+  
+}
+
   return (
-    <div>
-      this is reg
-    </div>
+    <FormContainer>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="brand">
+          <img src="" alt="logo" />
+          <h1>ChatSphere</h1>
+        </div>
+        <input type="text" placeholder='Username' name='username' onChange={e => handleChange(e)} />
+        <input type="email" placeholder='Email' name='email' onChange={e => handleChange(e)} />
+        <input type="password" placeholder='Password' name='password' onChange={e => handleChange(e)} />
+        <input type="password" placeholder='Confirm password' name='confirmPassword' onChange={e => handleChange(e)} />
+        <button type='submit'>Create User</button>
+        <Link to="/login"><span>already have an account ?</span></Link>
+      </form>
+    </FormContainer>
   )
 }
+
+const FormContainer = styled.div``;
 
 export default Register
